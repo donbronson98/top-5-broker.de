549a805d7504a28a0a8d95194b5758ba105f3a82
Sascha <79339263+donbronson98@users.noreply.github.com>
2022-12-16 11:39:55 +0100

    added an mobile menu with animation

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-document.getElementsByClassName("mobile-bottom-nav").onclick = function() {
-    myFunction()
-}
+document.getElementsByClassName('mobile-bottom-nav')[0].onclick = function(){mobileMenu()}
 
-function myFunction() {
-    var a = document.getElementsByClassName("mobile-bottom-nav");
-    a.style.backgroundColor="yellow";
+function mobileMenu() {
+    if(window.getComputedStyle(document.getElementsByClassName('mobile-menu')[0], null).display=='none') {
+        document.getElementsByClassName('mobile-menu')[0].style.display = 'flex';
+        
+        //mobile bar animation
+        document.getElementsByClassName('mobile-bar')[1].style.backgroundColor = 'transparent';
+        document.getElementsByClassName('mobile-bar')[0].style.transform = 'translateY(10px) rotate(45deg)';
+        document.getElementsByClassName('mobile-bar')[2].style.transform = 'translateY(-10px) rotate(-45deg)';
+    }
+    else {
+        document.getElementsByClassName('mobile-menu')[0].style.display = 'none';
+
+        //mobile bar animation
+        document.getElementsByClassName('mobile-bar')[1].style.backgroundColor = '#000000';
+        document.getElementsByClassName('mobile-bar')[0].style.transform = 'none';
+        document.getElementsByClassName('mobile-bar')[2].style.transform = 'none';
+    }
 }
